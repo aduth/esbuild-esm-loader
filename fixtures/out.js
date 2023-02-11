41de8d6869150ccaf0c509c687562f392552292a
Andrew Duthie <andrew@andrewduthie.com>
2023-02-11 13:59:04 -0500

    Bump dependencies to latest

--- fixtures/out.js
+++ fixtures/out.js
@@ -1,3 +1,3 @@
-import {createElement as forTest} from "react";
-import {renderToStaticMarkup} from "react-dom/server";
+import { createElement as forTest } from "react";
+import { renderToStaticMarkup } from "react-dom/server";
 console.log(renderToStaticMarkup(/* @__PURE__ */ forTest("div", null, "Hello World!")));
